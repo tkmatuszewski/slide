d3d407461d30fe30d7b35130c588673aa9a1d133
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-22 00:25:21 +0200

    Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button({name, className}) {
+    return (
+        <button className={className}>
+            {name}
+        </button>
+    )
+};
