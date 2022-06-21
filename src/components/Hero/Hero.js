da1ad2e3c47ca38a6d5c4da0d88726eca6ef46fb
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-22 00:19:22 +0200

    Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import hero__vid from "../../images/hero__vid.mp4";
+
+export default function Hero() {
+  return (
+    <section id="home">
+      <div className="hero__cnt">
+        <div className="hero__inner__cnt">
+          <h1 id="logo">Ride!</h1>
+          <div className="hero__bg">
+            <video
+              className="hero__vid"
+              src={hero__vid}
+              type="video/mp4"
+              autoPlay={true}
+              preload="auto"
+              loop={true}
+              muted={true}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
