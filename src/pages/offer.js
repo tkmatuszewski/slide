167a4259ea9ff0c4db2934c2081da148c7fe761e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-22 00:43:03 +0200

    Offer page

---
+++ src/pages/offer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import ProgramList from '../components/ProgramList/ProgramList';
+
+const offer = () => {
+    return (
+        <>
+            <Header />
+            <ProgramList />
+            <Footer/>
+        </>
+    )
+}
+
+export default offer;
