649178f630319fe3bd7c673d50369a078796a51c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-21 23:46:33 +0200

    Homepage sections contnent

---
+++ src/constants/mainContent.js
@@ -0,0 +1,30 @@
+const about = {
+  name: "About",
+  desc: "",
+  btn: "Our story",
+  link: "/about",
+};
+
+const offer = {
+  name: "Offer",
+  desc: `We believe there is no such thing as "one road" for everyone. That's the reason we choose the private lessons model. We can customize the time, place, level and equipment to fit your needs and preferences. You are in our focus no matter if it's your first time on the board or you've been ripping for years. We'll give you tips, homework exercises and invite you to our BBQ zone on the weekend.`,
+  btn: "Show offers",
+  link: "/offer",
+  img: ""
+};
+
+const team = {
+  name: "Team",
+  desc: "We have the right people on the right place. We're giving the best of us from customer care and lesson arrangement to ripping on the beach and water itself!",
+  btn: "Meet our team",
+  link: "/team",
+};
+
+const contact = {
+  name: "Contact",
+  desc: "Got any questions? Want to know any specifics or you want to make a reservation already? Don't hesitate to write us! ",
+  btn: "Contact Us",
+  link: "/contact"
+}
+
+export const sectionsContent = [about, offer, team, contact];
