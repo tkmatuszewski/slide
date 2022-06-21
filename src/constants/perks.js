8d5f6e2f45425d1db96c366e98d0f524d72e70a5
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-22 00:36:59 +0200

    offer content

---
+++ src/constants/perks.js
@@ -0,0 +1,42 @@
+export const perks = [
+  {
+    name: "Gear",
+    description:
+      "We provide you with a board suitable for you based on your skill level and ergonomics.",
+  },
+  {
+    name: "Instructor",
+    description:
+      "Your lesson is private period. To maximize participant progress we obey the rule one student one teacher. Only then supervisor won't miss a thing",
+  },
+  {
+    name: "Analytics",
+    description:
+      "We make some use of the tech on the water to enhance learning proces and give you some feedback.",
+  },
+  {
+    name: "Safety",
+    description:
+      "There are no shortcuts here so if conditions require speedboat we've got it covered for you.",
+  },
+  {
+    name: "Family Vibe",
+    description:
+      "Distance is not our style. We want to expand our surf oriented family and you'll be a part of it.",
+  },
+  {
+    name: "Comunity",
+    description:
+      "Join the comunity of open minded surfing enthusiasts and exchange the session impressions.",
+  },
+  {
+    name: "Events",
+    description:
+      "Multiple surfing events like torunaments or surf camps are organised for our students.",
+  },
+  {
+    name: "Keep in shape",
+    description:
+      "You know surfing is no chess! Some muscle work and balance are essential and we are prepared to help you improving them both.",
+  },
+];
---
+++ src/constants/programs.js
@@ -0,0 +1,20 @@
+export const programs = [
+  {
+    id: 1,
+    name: "Sea Horse",
+    level: "Begginer",
+    color: "green",
+  },
+  {
+    id: 2,
+    name: "Barracuda",
+    level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    id: 3,
+    name: "Great White",
+    level: "Expert",
+    color: "red",
+  },
+];
