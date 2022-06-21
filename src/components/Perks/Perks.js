fc54e6bc48e4c8918ef0a1356f7175684b12611c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-22 00:37:52 +0200

    Perks component

---
+++ src/components/Perks/Perks.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { perks } from "../../constants/perks";
+
+export default function OfferPerks() {
+  return (
+    <div className="perks">
+      <ul className="perks__cnt">
+        <h2 className="perks__title">Our perks</h2>
+        {perks.map((el) => {
+          return (
+            <li className="perk__card" key={el.name}>
+              <strong>{el.name}</strong>
+              <p>{el.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
