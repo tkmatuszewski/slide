09aced4aad23d6e6779587d830c27e3c1b917b1c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-30 23:34:18 +0200

    Feat adjusted to photos

--- src/components/Feat/Feat.js
+++ src/components/Feat/Feat.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Button from '../Button/Button';
 import { Link } from "gatsby";
+// import { StaticImage } from 'gatsby-plugin-image';
 
-const Feat = ({ feat }) => {
+const Feat = ({ feat, children }) => {
   return (
     <>
       <section className="feat__wrapper">
+        {children}
         <div className="feat__cnt">
-          <div className="feat">
+          {/* {feat.img__side ? <div className="feat__circle">{children}</div> : null} */}
+          <div className={feat.class}>
             <strong>{feat.name}</strong>
             <p>{feat.desc}</p>
-            <Link to={feat.link}>
-              <Button name={feat.btn} className={"button"} />
-            </Link>
+            {feat.link ? (
+              <Link to={feat.link}>
+                <Button name={feat.btn} className={"button"} />
+              </Link>
+            ) : null}
           </div>
         </div>
       </section>
