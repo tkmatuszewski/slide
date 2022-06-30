46c31933f855af0218fda82bce4b894d9abeb696
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-06-30 23:31:53 +0200

    Staff redesigned. No side scrolling

--- src/components/Staff/Staff.js
+++ src/components/Staff/Staff.js
@@ -1,25 +1,31 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { team } from '../../constants/team';
 
 export default function Staff() {
   return (
     <div className="staff">
+      <div className="staff__head">
+        <strong>Team</strong>
+        {/* <StaticImage src="../../images/about.png" alt="" /> */}
+        <p>
+          We are not just a company. We are a pack friends looking for new
+          people to share our passion with.
+        </p>
+      </div>
       <div className="staff__cnt">
-        <div className="staff__head">
-          <strong>Crew</strong>
-        </div>
         <div className="box__cnt">
           {team.map((el) => {
             return (
               <div className="box" key={el.id}>
-                <img
-                  className="box__img"
+                <StaticImage
+                  // className="box__img"
                   src={el.url}
                   alt="portrait of a man"
                 />
                 <div className="box__data">
                   <span className="box__name">{el.name}</span>
-                  <span>{el.role}</span>
+                  <span className="box__role">{el.role}</span>
                 </div>
               </div>
             );
