dcfc0754cbdbcfdef1621ed1ad5b617f3350ceee
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-03 23:58:48 +0200

    Team content filled

--- src/constants/team.js
+++ src/constants/team.js
@@ -1,30 +1,34 @@
 export const team = [
   {
-      id: 1,
-      url: "https://images.unsplash.com/photo-1450133064473-71024230f91b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      name: "Keith",
-      exp: "10",
-      role: "Founder"
-    },
+    id: 1,
+    url: "https://images.unsplash.com/photo-1450133064473-71024230f91b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    name: "Keith",
+    exp: "10",
+    role: "Co-founder",
+    det: "Some may say being raised by a pro surfer father there is no other way around. I spent my childhood on the surfboard and running around the beach. I started compeating when I was 10 years old and spend a decade on World Tour. I esspecialy like working with kids as it reminds me myself and my father back in the days. SEE YOU SOON ;-)",
+  },
   {
-      id: 2,
-      url: "https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-      name: "Josh",
-      exp: "15",
-      role: "Founder"
-    },
+    id: 2,
+    url: "https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    name: "Josh",
+    exp: "15",
+    role: "Co-founder",
+    det: "I love exploring new places, conditions getting to know new people. I've travelled the world for 4 years along with my surf board. I have a lot to tell you and show you. If you are up for a surf trip I'm allways happy to join and help with organisation. Aloha! ;-)",
+  },
   {
-      id: 3,
-      url: "https://images.unsplash.com/photo-1484608856193-968d2be4080e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      name: "Jessie",
-      exp: "8",
-      role: "Receptionist"
-    },
+    id: 3,
+    url: "https://images.unsplash.com/photo-1484608856193-968d2be4080e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "Jessie",
+    exp: "8",
+    role: "Receptionist",
+    det: "Hey! I run the office as my job and surf in every possible break. I'll arrange everything to suit your needs. Want a private course? No problem. Surf trip? Done! I'm here to answer your questions and help you organise the surfing experiance you dream of. -Have a great day! ;-)",
+  },
   {
-      id: 4,
-      url: "https://images.unsplash.com/photo-1578251122719-faf0727a76de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      name: "Swell",
-      exp: "5",
-      role: "Beachboy"
-    }
-  ]
+    id: 4,
+    url: "https://images.unsplash.com/photo-1578251122719-faf0727a76de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "Swell",
+    exp: "5",
+    role: "Beachboy",
+    det: "You can have a great time on the water. I'll take care of your belongings and you can be sure I'll make some noise when it's needed. I hope you'll remember my duties when the after session BBQ time comes. WOOF! ;-)",
+  },
+];
