faff5047dc8062516d3bf9acc002f94f82c08908
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-04 00:04:05 +0200

    Feat cleaned up

--- src/components/Feat/Feat.js
+++ src/components/Feat/Feat.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Button from '../Button/Button';
 import { Link } from "gatsby";
-// import { StaticImage } from 'gatsby-plugin-image';
 
 const Feat = ({ feat, children }) => {
+  
   return (
     <>
       <section className="feat__wrapper">
         {children}
         <div className="feat__cnt">
-          {/* {feat.img__side ? <div className="feat__circle">{children}</div> : null} */}
           <div className={feat.class}>
             <strong>{feat.name}</strong>
             <p>{feat.desc}</p>
