7bf1597fc94cde638d6d50b4a7d1113b1b6fe3a7
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-03 23:59:31 +0200

    Team subsite redesigned

--- src/components/Staff/Staff.js
+++ src/components/Staff/Staff.js
@@ -1,37 +1,34 @@
-import { StaticImage } from 'gatsby-plugin-image';
+import { MainImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { team } from '../../constants/team';
 
 export default function Staff() {
   return (
-    <div className="staff">
-      <div className="staff__head">
+    <>
+      <div className="staff">
         <strong>Team</strong>
-        {/* <StaticImage src="../../images/about.png" alt="" /> */}
         <p>
           We are not just a company. We are a pack friends looking for new
           people to share our passion with.
         </p>
       </div>
-      <div className="staff__cnt">
-        <div className="box__cnt">
+      <div className='staff__bbq'>
+        <MainImage src="https://unsplash.com/photos/x9I-6yoXrXE" alt=""/>
+      </div>
+        <div className="staff__box__cnt">
           {team.map((el) => {
             return (
               <div className="box" key={el.id}>
-                <StaticImage
-                  // className="box__img"
-                  src={el.url}
-                  alt="portrait of a man"
-                />
+                <MainImage src={el.url} alt="" className="team__img"/>
                 <div className="box__data">
                   <span className="box__name">{el.name}</span>
                   <span className="box__role">{el.role}</span>
                 </div>
+                <p className="box__det">{el.det}</p>
               </div>
             );
           })}
         </div>
-      </div>
-    </div>
+    </>
   );
 }
