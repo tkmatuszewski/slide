6b43ce877ba5e1d82ddffa6ed43c7d1aaab0fb26
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-05 23:59:08 +0200

    Programs content

--- src/constants/programs.js
+++ src/constants/programs.js
@@ -1,20 +1,42 @@
 export const programs = [
   {
     id: 1,
-    name: "Sea Horse",
-    level: "Begginer",
-    color: "green",
+    level: "Beginer",
+    entry: ["Basically none just enthusiastic altitude"],
+    master: [
+      "Safety rules",
+      "Paddling on the water",
+      "Standing up on the board",
+      "Catching first waves",
+    ],
+    bg: "linear-gradient(to right,#c33764, #1d2671)",
   },
   {
     id: 2,
-    name: "Barracuda",
     level: "Intermediate",
-    color: "yellow",
+    entry: ["You know how to paddle on the board", "You can catch a wave"],
+    master: [
+      "Picking the right wave",
+      "Board handling",
+      "Cavring and bottom turns",
+    ],
+    bg: "linear-gradient(to right,#43cea2, #185a9d)",
   },
   {
     id: 3,
-    name: "Great White",
     level: "Expert",
-    color: "red",
+    entry: [
+      "You do not fear even bigger wave",
+      "You know how to deal with current",
+      "You catch and ride most of the waves ",
+    ],
+    master: [
+      "Some radical stuff!",
+      "Surf style tips",
+      "Jumps",
+      "Tube riding",
+      "Competition ready surfer",
+    ],
+    bg: "yellow",
   },
 ];
