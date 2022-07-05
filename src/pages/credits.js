2c808b24fa214ee6ad43415942c3c9bb283a9e0a
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-06 00:05:52 +0200

    credits added

---
+++ src/pages/credits.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+export default function Credits() {
+  return (
+    <>
+      <Header />
+      <h1>Credits</h1>
+      cons8.com Photo by Max Ravier:
+      https://www.pexels.com/photo/two-men-swimming-3348391/
+      <Footer />
+    </>
+  );
+}
