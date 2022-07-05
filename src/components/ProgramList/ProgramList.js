2b6d39d74a086db08484f77957c828b540a032d4
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-05 23:58:25 +0200

    ProgramList in one box form

--- src/components/ProgramList/ProgramList.js
+++ src/components/ProgramList/ProgramList.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import surfer from '../../images/surfer.jpg';
+import React, {lazy, useState} from 'react';
 import { programs } from '../../constants/programs';
 import Perks from '../Perks/Perks';
 import { Program } from '../Program/Program';
+import { MainImage, StaticImage } from 'gatsby-plugin-image';
 
 const ProgramList =()=> {
 
+  const [selected, setSelected] = useState("Beginer")
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSelected(e.target.value)
+  }
+
   return (
     <div className="offer">
-      <img src={surfer} alt={"Surfer riding a wave"} />
-      <div className="offer__cnt">
+      <div className="offer__wrapper">
+        <StaticImage src={'../../images/surfer.jpg'} alt={"Surfer riding a wave"}/>
         <div className="offer__hero">
           <span>More.</span>
           <ul>
@@ -21,20 +28,51 @@ const ProgramList =()=> {
           </ul>
         </div>
       </div>
-    <Perks/>
-      <div className='offer__slogan'>
-          <h2>Sign up for our program and be a part of bigger picture.</h2>
+      <Perks />
+      <div className="offer__slogan">
+        <h2>We will take you from zero to hero.</h2>
+        <p>You can walk and you can swim right? So now we'll teach you how to surf. It's entertaining, positive and versetile activity. Even the smallest adepts playing with the board have loads of fun.</p>
       </div>
-      <div className='offer__programs__cnt'>
-          <div className='offer__programs'>
-          {programs.map((program) => {
-              return (
-                <Program id={program} key={program.id}/>
-              )
-              })
-            } 
-          </div>
+      <div className="offer__programs__cnt">
+        <div className="offer__level">
+          <strong>Level</strong>
+          <button
+            value="Beginer"
+            onClick={(e) => {
+              handleChange(e);
+            }}
+          >
+            Beginer
+          </button>
+          <button
+            value="Intermediate"
+            onClick={(e) => {
+              handleChange(e);
+            }}
+          >
+            Intermediate
+          </button>
+          <button
+            value="Expert"
+            onClick={(e) => {
+              handleChange(e);
+            }}
+          >
+            Expert
+          </button>
         </div>
+        <div className='offer__right'>
+          {/* <MainImage src={"https://unsplash.com/photos/cYdfGZrTPnk"} alt=""/> */}
+         {programs.map(program => {
+            if (program.level === selected) {
+              return <Program id={program} key={program.id}/>
+            }
+          })}
+        </div>
+      </div>
+      <div>
+        Contact us for booking and futher information.
+      </div>
     </div>
   );
 }
