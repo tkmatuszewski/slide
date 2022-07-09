872099479ca0216755c43126fec003664eae0d9f
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-09 14:17:10 +0200

    Netlify build error fix

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import * as React from "react";
 
-const Button =({name, className})=> {
+const Button =(name, className)=> {
     return (
         <button className={className}>
             {name}
