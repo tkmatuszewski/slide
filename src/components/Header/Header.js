ec9beea04acff44b2aba4cbba42f46a0c2753119
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-09 13:09:09 +0200

    Header hamburger menu for mobile devices to be finnished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "gatsby";
 
 export default function Header() {
+
+  const [showMenu, setShowMenu]= useState(false)
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className="header">
-      <div className="nav__cnt">
+      {/* <div className="nav__cnt">
+        <button className="nav__btn" onClick={(e) => handleClick(e)}>
+          Menu
+        </button>
         <nav className="nav">
-          <Link to='/about'>About</Link>
-          <Link to='/offer'>Offer</Link>
-          <Link to='/' id="log">
+          <Link to="/" className='home'>Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/offer">Offer</Link>
+          <Link to="/" id="log">
             Slide.
           </Link>
-          <Link to='/team'>Team</Link>
-          <Link to='/contact'>Contact</Link>
+          <Link to="/team">Team</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
-      </div>
+      </div> */}
     </div>
   );
 }
