b10ba884c616ac437f164459aa0050acec545c1e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-11 00:55:29 +0200

    Contribution component and page

---
+++ src/components/Credits/Credits.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contiburion =()=> {
+  return (
+    <div className="credits">
+      <div>
+        <strong>Icons</strong>
+        <span>Icons8</span>
+      </div>
+      <div>
+        <strong>Photos</strong>
+            <span>unsplash</span>
+            <span>pexels</span>  
+      </div>
+      <div>
+        <strong>Fonts</strong>
+        <span>Google Fonts</span>
+      </div>
+    </div>
+  );
+}
+
+export default Contiburion;
--- src/pages/credits.js
+++ src/pages/credits.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import Contiburion from '../components/Credits/Credits';
 
 export default function Credits() {
   return (
     <>
       <Header />
-      <h1>Credits</h1>
-      cons8.com Photo by Max Ravier:
-      https://www.pexels.com/photo/two-men-swimming-3348391/
+      <Contiburion/>
       <Footer />
     </>
   );
