67d412921005637a175482112bbecc0b4d341591
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-11 00:50:31 +0200

    Offer section responsive

--- src/components/ProgramList/ProgramList.js
+++ src/components/ProgramList/ProgramList.js
@@ -2,7 +2,8 @@ import React, {lazy, useState} from 'react';
 import { programs } from '../../constants/programs';
 import Perks from '../Perks/Perks';
 import { Program } from '../Program/Program';
-import { MainImage, StaticImage } from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image';
+import './ProgramList.scss';
 
 const ProgramList =()=> {
 
@@ -16,7 +17,10 @@ const ProgramList =()=> {
   return (
     <div className="offer">
       <div className="offer__wrapper">
-        <StaticImage src={'../../images/surfer.jpg'} alt={"Surfer riding a wave"}/>
+        <StaticImage
+          src={"../../images/surfer.jpg"}
+          alt={"Surfer riding a wave"}
+        />
         <div className="offer__hero">
           <span>More.</span>
           <ul>
@@ -31,47 +35,57 @@ const ProgramList =()=> {
       <Perks />
       <div className="offer__slogan">
         <h2>We will take you from zero to hero.</h2>
-        <p>You can walk and you can swim right? So now we'll teach you how to surf. It's entertaining, positive and versetile activity. Even the smallest adepts playing with the board have loads of fun.</p>
+        <p>
+          You can walk and you can swim right? So now we'll teach you how to
+          surf. It's entertaining, positive and versetile activity. Even the
+          smallest adepts playing with the board have loads of fun.
+        </p>
       </div>
       <div className="offer__programs__cnt">
-        <div className="offer__level">
-          <strong>Level</strong>
-          <button
-            value="Beginer"
-            onClick={(e) => {
-              handleChange(e);
-            }}
-          >
-            Beginer
-          </button>
-          <button
-            value="Intermediate"
-            onClick={(e) => {
-              handleChange(e);
-            }}
-          >
-            Intermediate
-          </button>
-          <button
-            value="Expert"
-            onClick={(e) => {
-              handleChange(e);
-            }}
-          >
-            Expert
-          </button>
+        <div className="offer__left">
+          <div className="offer__level">
+            <strong>Level</strong>
+            <button
+              value="Beginer"
+              onClick={(e) => {
+                handleChange(e);
+              }}
+            >
+              Beginer
+            </button>
+            <button
+              value="Intermediate"
+              onClick={(e) => {
+                handleChange(e);
+              }}
+            >
+              Intermediate
+            </button>
+            <button
+              value="Expert"
+              onClick={(e) => {
+                handleChange(e);
+              }}
+            >
+              Expert
+            </button>
+          </div>
+          <label className="offer__slct">
+            Your experiance
+            <select onChange={(e) => handleChange(e)}>
+              <option value="Beginer">beginer</option>
+              <option value="Intermediate">intermediate</option>
+              <option value="Expert">expert</option>
+            </select>
+          </label>
         </div>
-        <div className='offer__right'>
-          {/* <MainImage src={"https://unsplash.com/photos/cYdfGZrTPnk"} alt=""/> */}
-         {programs.map(program => {
+        <div className="offer__right">
+          {programs.map((program) => {
             if (program.level === selected) {
-              return <Program id={program} key={program.id}/>
+              return <Program id={program} key={program.id} />;
             }
           })}
         </div>
-      </div>
-      <div>
-        Contact us for booking and futher information.
       </div>
     </div>
   );
