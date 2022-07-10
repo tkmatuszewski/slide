29b3516a8b5b1e8a6d2e7b8b4851de837fdf44bb
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-11 00:47:07 +0200

    mainContent unsused params erased

--- src/constants/mainContent.js
+++ src/constants/mainContent.js
@@ -1,7 +1,6 @@
 export const hi = {
-  name: "Intro. Get to know the feeling",
+  name: `Intro. Get to know the feeling`,
   desc: "A whole lot of water surrounds you, and gust of wind hits your face as the wave breaks right behind your back. Adrenaline starts to flow along with the wide smile on your face. You're picking up speed and sliding down the wave. There is no mistake. It's surfing baby!",
-  class: "feat wide",
   img__center: true
 }
 
@@ -10,7 +9,6 @@ export const about = {
   desc: "We were all raised on the beach and surfing had a massive impact on our lives. We know about the difficult start, injuries and hours of training from first person perspective. Our proffesional background never fliped the main idea that connected us in the first place. Friendship and fun are always with us.",
   btn: "Our story",
   link: "/about",
-  class: "feat",
   img : true
 };
 
@@ -20,21 +18,19 @@ export const offer = {
   btn: "Show offers",
   link: "/offer",
   img__center: true,
-  class: "feat"
+  
 };
 
 export const team = {
   name: "Team",
   desc: "We have the right people on the right place. We're giving the best of us from customer care and lesson arrangement to ripping on the beach and water itself!",
   btn: "Meet our team",
-  link: "/team",
-  class: "feat wide"
+  link: "/team"
 };
 
 export const contact = {
   name: "Contact",
   desc: "Got any questions? Want to know any specifics or you want to make a reservation already? Don't hesitate to write us! ",
   btn: "Contact Us",
-  link: "/contact",
-  class: "feat"
+  link: "/contact"
 };
