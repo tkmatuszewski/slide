a41d563a3471659a0efc7baa53ddab87d218199e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2022-07-11 00:53:18 +0200

    css import fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styles from "../styles/styles.scss";
+// import "../../styles.scss";
 import Header from '../components/Header/Header';
 import Main from '../components/Main/Main';
 import Footer from "../components/Footer/Footer";
